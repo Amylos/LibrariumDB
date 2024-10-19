e8b9c6778b2e7a5aa5dd6c886034c169ed42c078
Amylos <andrewgarnierpro@gmail.com>
2024-10-19 23:29:46 +0200

    first creation of users in mongoDB

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,15 +1,22 @@
+const usersModel = require('../models/users.model');
 const UsersModel = require('../models/users.model');
 
 
 
 
 module.exports.setUsers = async(req,res)=>{
-
-    if(req.body.message){
-        res.json({message:"setUsers"});
-    }
-    else{
-        res.status(400).json({message: "Il manque un message"})
+    console.log(req.body)
+    if(!req.body.message){
+        // res.status(400).json({message: "Messages is missing"})
     }
 
-}
+    const user = await usersModel.create({
+        pseudo: req.body.pseudo,
+        password: req.body.password
+    });
+    res.status(200).json(user);
+};
+
+
+
+
